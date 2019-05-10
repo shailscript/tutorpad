400e368b73245fab4f91323d0f3c76f619004dd7
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-10 16:28:58 -0400

    [FIX] missing router import added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from '@/router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
