d7dbcd96005fdadbf464f4774e69c1d1e9410582
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-10 19:18:03 -0400

    Added sticky actions

--- src/views/Finder.vue
+++ src/views/Finder.vue
@@ -2,8 +2,11 @@
   <div>
     <h2 class="py-2 px-3 font-serif font-hairline text-3xl">Finder</h2>
     <p class="px-3 py-2 font-sans font-thin text-lg text-grey-dark">Select your preference from the options below, so we can assist you better</p>
-
     <cities></cities>
+    <div class="sticky pin-x pin-b bg-grey-light p-4 flex justify-between">
+      <button class="w-full mr-1 py-2 font-hairline text-xl bg-blue-darkest text-white rounded">Back</button>
+      <button class="w-full ml-1 py-2 font-hairline text-xl bg-blue-darkest text-white rounded">Next</button>
+    </div>
   </div>
 </template>
 
