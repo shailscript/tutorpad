47a8c07c762c27a593a834fcc1abd43f4c0393ff
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-10 17:16:58 -0400

    Finder view added

---
+++ src/views/Finder.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2 class="py-2 px-3 font-serif font-hairline text-3xl">Finder</h2>
+    <p class="px-3 py-2 font-sans font-thin text-lg text-grey-dark">Select your preference from the options below, so we can assist you better</p>
+
+    <cities></cities>
+  </div>
+</template>
+
+<script>
+import Cities from '@/components/Cities.vue'
+
+export default {
+  name: 'Finder',
+  components: {
+    Cities
+  }
+}
+</script>
+
+<style>
+
+</style>
+
