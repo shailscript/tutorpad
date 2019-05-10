54269536ac9588597d403f66a793527ac060bc69
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-10 17:01:31 -0400

    Spearated navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="nav flex justify-between p-5">
+    <div>
+      <font-awesome-icon icon="bars" size="2x"></font-awesome-icon>
+    </div>
+    <div class="logo">
+      <font-awesome-icon icon="chalkboard-teacher" size="2x"></font-awesome-icon>
+    </div>
+    <div>
+      <font-awesome-icon icon="user" size="2x"></font-awesome-icon>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
