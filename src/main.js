36faa36b21f3aa4148ac56a1b4b0da9eaf96c872
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-10 00:01:12 -0400

    fa helper references added

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+//import { far } from '@fortawesome/free-regular-svg-icons'
+//import { fab } from '@fortawesome/free-brands-svg-icons'
 
 library.add(fas)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
