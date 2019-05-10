08dea92cf6d3e59fcf7515987301efab182e5697
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-10 17:02:20 -0400

    Router view added and app cleaned

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,12 @@
 <template>
   <div id="app">
-    <Hero/>
-    <ActionButton/>
-    <div id="content" class="">
-      <h2 class="py-1 px-3 font-serif font-thin text-1xl">Featured Tuition</h2>
-      <p class="px-3 font-sans font-thin text-lg text-grey-dark">These are editor's recommendations</p>
-      <div id="cards" class="my-5">
-        <TuitionCard/>
-        <TuitionCard/>
-        <TuitionCard/>
-        <TuitionCard/>
-      </div>
-    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Hero from '@/components/Hero.vue'
-import ActionButton from '@/components/ActionButton.vue'
-import TuitionCard from '@/components/TuitionCard.vue'
-
 export default {
-  name: 'app',
-  components: {
-    Hero,
-    ActionButton,
-    TuitionCard
-  }
+  name: 'app'
 }
 </script>
 
