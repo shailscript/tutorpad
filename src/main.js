e1a82dec9901fec872ffc9154db0b4d21c32c1a6
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-09 23:41:58 -0400

    [FIX] fa component import

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(fas)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
