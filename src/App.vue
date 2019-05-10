ffa4e58310b37b3f8db7fb819cf4060e8f77b2be
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-09 23:42:25 -0400

    Separated hero component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,6 @@
 <template>
   <div id="app">
-    <div id="hero-bg" class="hero bg-cover w-screen flex flex-col">
-    </div>
-    <div id="hero" class="flex w-screen flex-col">
-      <div class="nav flex justify-between p-5">
-        <div>
-          <font-awesome-icon icon="bars" size="2x"></font-awesome-icon>
-        </div>
-        <div class="logo">
-          <font-awesome-icon icon="chalkboard-teacher" size="2x"></font-awesome-icon>
-        </div>
-        <div>
-          <font-awesome-icon icon="user" size="2x"></font-awesome-icon>
-        </div>
-      </div>
-      <div class="w-3/4 my-auto">
-        <p class="font-hairline font-serif px-5 text-4xl text-white">Your hunt for a tuition starts here.</p>
-      </div>
-    </div>
+    <Hero/>
     <div id="actionButton" class="flex justify-center mb-20">
       <div class="-my-10 px-28 primary-gradient rounded-lg flex justify-center">
         <p class="font-hairline text-white text-2xl my-auto pr-3">Try finder</p>
@@ -28,8 +11,12 @@
 </template>
 
 <script>
+import Hero from '@/components/Hero.vue'
 export default {
   name: 'app',
+  components: {
+    Hero
+  }
 }
 </script>
 
@@ -38,22 +25,8 @@ export default {
   @tailwind components;
   @tailwind utilities;
 
-  #hero-bg{
-    background-image: url("./assets/hero.jpeg");
-    height: 400px;
-    position: absolute;
-    z-index: -5;
-  }
-  #hero{
-    background-color: rgba(0,0,0, 0.4);
-    position: relative;
-    height: 400px;
-    z-index: -2;
-  }
-  .nav {
-    color: white;
-  }
-  .primary-gradient{
-    background: linear-gradient(283.71deg, #292E49, #536976);
-  }
+
+.primary-gradient{
+  background: linear-gradient(283.71deg, #292E49, #536976);
+}
 </style>
