f77bc5cf6b322b52392a7417a4e732aadf291645
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-10 17:00:48 -0400

    Guest layout added

---
+++ src/views/layouts/Guest.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+  name: 'Guest',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style scoped>
+.nav {
+  color: black;
+}
+</style>
+
