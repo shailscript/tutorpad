a6a685732700e504a39e685d2ae26bb54328ce9d
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-09 23:52:34 -0400

    Separated action button to a component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <template>
   <div id="app">
     <Hero/>
-    <div id="actionButton" class="flex justify-center mb-20">
-      <div class="-my-10 px-28 primary-gradient rounded-lg flex justify-center">
-        <p class="font-hairline text-white text-2xl my-auto pr-3">Try finder</p>
-        <font-awesome-icon icon="arrow-right" size="1x" :style="{color: 'white'}" class="my-auto"></font-awesome-icon>
-      </div>
-    </div>
+    <ActionButton/>
   </div>
 </template>
 
 <script>
 import Hero from '@/components/Hero.vue'
+import ActionButton from '@/components/ActionButton.vue'
+
 export default {
   name: 'app',
   components: {
-    Hero
+    Hero,
+    ActionButton
   }
 }
 </script>
@@ -24,9 +22,4 @@ export default {
   @tailwind preflight;
   @tailwind components;
   @tailwind utilities;
-
-
-.primary-gradient{
-  background: linear-gradient(283.71deg, #292E49, #536976);
-}
 </style>
---
+++ src/components/ActionButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="actionButton" class="flex justify-center mb-20">
+    <div class="-my-10 px-28 primary-gradient rounded-lg flex justify-center">
+      <p class="font-hairline text-white text-2xl my-auto pr-3">Try finder</p>
+      <font-awesome-icon icon="arrow-right" size="1x" :style="{color: 'white'}" class="my-auto"></font-awesome-icon>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ActionButton',
+}
+</script>
+
+<style scoped>
+.primary-gradient{
+  background: linear-gradient(283.71deg, #292E49, #536976);
+}
+</style>
+
