f9a364de2ebd8144eba12b456d6b55a30b9666f2
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-10 17:01:04 -0400

    Link to finder added

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="actionButton" class="flex justify-center mb-20">
-    <div class="-my-10 px-28 primary-gradient rounded-lg flex justify-center">
-      <p class="font-hairline text-white text-2xl my-auto pr-3">Try finder</p>
-      <font-awesome-icon icon="arrow-right" size="1x" :style="{color: 'white'}" class="my-auto"></font-awesome-icon>
-    </div>
+    <router-link to="/finder">
+      <div class="-my-10 px-28 primary-gradient rounded-lg flex justify-center">
+        <p class="font-hairline text-white text-2xl py-6 pr-3">Try finder</p>
+        <font-awesome-icon icon="arrow-right" size="1x" :style="{color: 'white'}" class="my-auto"></font-awesome-icon>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -16,6 +18,9 @@ export default {
 <style scoped>
 .primary-gradient{
   background: linear-gradient(283.71deg, #292E49, #536976);
+}
+a{
+  text-decoration: none;
 }
 </style>
 
