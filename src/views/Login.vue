a068396c74af3c49bbec3bab96b9462928aeac6a
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-10 19:16:23 -0400

    Added submit button

---
+++ src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2 class="py-2 mx-5 font-serif font-hairline text-3xl">Login</h2>
+    <div>
+       <p class="my-2 mx-5 font-sans text-lg text-grey-dark">Name</p>
+       <input class="w-full mx-5 p-4 border border-grey-light rounded-lg" type="text" name="" id="" placeholder="Ex. John Doe">
+       <p class="my-2 mx-5 font-sans text-lg text-grey-dark">Name</p>
+       <input class="w-full mx-5 p-4 border border-grey-light rounded-lg" type="text" name="" id="" placeholder="Ex. John Doe">
+    </div>
+    <div class="sticky pin-x pin-b bg-grey-light p-4 flex justify-around">
+      <button class="w-full py-2 font-hairline text-xl bg-blue-darkest text-white rounded">Submit</button>
+    </div>
+  </div>
+</template>
