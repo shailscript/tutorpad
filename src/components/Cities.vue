a628ecfc4dab2bb02c9d8b208765b82a9597d99e
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-10 17:43:44 -0400

    Cities component added

---
+++ src/components/Cities.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="py-2">
+    <div class="bg-blue-darkest w-32">
+      <p class="text-white">Ahmedabad</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
