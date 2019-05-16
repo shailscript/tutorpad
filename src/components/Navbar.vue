3935b56f84b49ec51ea679f76c366e40dd4b9d04
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-16 14:50:48 -0400

    Prop validation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="flex justify-between p-5">
     <div>
-      <font-awesome-icon icon="bars" size="2x" :class="textColor"></font-awesome-icon>
+      <font-awesome-icon icon="bars" size="2x" :class="className"></font-awesome-icon>
     </div>
     <router-link to='/'>
-      <font-awesome-icon icon="chalkboard-teacher" size="2x" :class="textColor"></font-awesome-icon>
+      <font-awesome-icon icon="chalkboard-teacher" size="2x" :class="className"></font-awesome-icon>
     </router-link>
     <router-link to='/login'>
-      <font-awesome-icon icon="user" size="2x" :class="textColor"></font-awesome-icon>
+      <font-awesome-icon icon="user" size="2x" :class="className"></font-awesome-icon>
     </router-link>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['text-color'],
+  props: {
+    className : {
+      type:String,
+      default: 'text-black'
+    }
+  },
   name: 'Navbar',
 }
 </script>
