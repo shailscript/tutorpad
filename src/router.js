c816258ef7ecea6464125f0f0c993c88965dbc75
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-19 23:42:44 -0400

    Prioritized core features

--- src/router.js
+++ src/router.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/views/Home.vue'
 import Finder from '@/views/Finder.vue'
-import Tuitions from '@/views/Tuitions.vue'
-import Login from '@/views/Login.vue'
 
 Vue.use(Router)
 
@@ -11,26 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/',
-      component: () => import('@/views/layouts/Guest.vue'),
-      children: [
-        {
-          path: 'finder',
-          component: Finder
-        },
-        {
-          path: 'login',
-          component: Login
-        },
-        {
-          path: 'tuitions',
-          component: Tuitions
-        }
-      ]
+      name: 'finder',
+      component: Finder
     },
   ]
 })
