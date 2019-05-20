debf34955da6418278d2d8b08832f4a4873fef69
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-20 09:04:27 -0400

    Header added and styles updated

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
-  <div id="app" class="min-h-screen">
-    <router-view class="min-h-full"></router-view>
+  <div id="app">
+    <div class="h-screen w-screen overflow-hidden flex flex-col">
+      <div class="text-center p-4">
+        <span class="inline-block bg-grey-light w-12 h-12 rounded">
+          <!-- Logo -->
+        </span>
+      </div>
+
+      <div class="flex-1 overflow-hidden">
+        <router-view/>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'app'
-}
-</script>
-
+<!--suppress CssInvalidAtRule -->
 <style>
-  @tailwind preflight;
-  @tailwind components;
-  @tailwind utilities;
+  @import url('https://cdn.jsdelivr.net/npm/tailwindcss@0.7.4/dist/tailwind.min.css');
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,500i,700&display=swap');
+  @import url('https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap');
+  @import url('https://use.fontawesome.com/releases/v5.8.2/css/all.css');
+  p {
+    font-family: 'Roboto', 'sans-serif';
+  }
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Roboto Slab', serif;
+  }
 </style>
