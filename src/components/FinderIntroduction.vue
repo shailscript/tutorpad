b97d79df6356ece50375b20edd63f350de4f9dbe
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-20 09:18:10 -0400

    First component for finder added

---
+++ src/components/FinderIntroduction.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col items-center justify-center px-4">
+    <h3 class="mb-4">What's this?</h3>
+
+    <p class="mb-8 text-center font-medium sm:text-lg leading-normal">
+      Select your preference from the options below, so we can assist you better. Click Next.
+    </p>
+
+    <img
+      class="w-full"
+      alt="Vector Image"
+      src="../assets/selection.svg"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FinderIntroduction',
+};
+</script>
