1173723274353a8bfa874131ef3cfc54bcb0eb53
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-20 09:02:35 -0400

    Finder added

--- src/router.js
+++ src/router.js
@@ -1,15 +1,23 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Finder from '@/views/Finder.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+// import Home from './views/Home.vue';
+import Finder from './views/Finder.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'finder',
-      component: Finder
+      name: 'home',
+      component: Finder, //for testing
     },
-  ]
-})
+    {
+      path: '/finder',
+      name: 'finder',
+      component: Finder,
+    },
+  ],
+});
