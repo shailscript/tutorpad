e84bdbabdeefc75f0394e7103b223cb9816bfe0b
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-21 14:43:48 -0400

    Tuition page layout implemented

--- src/views/Tuitions.vue
+++ src/views/Tuitions.vue
@@ -1,15 +1,20 @@
 <template>
-  <div>
-    Tuitions Index
+  <div class="h-full overflow-y-scroll">
+    <div class="container mx-auto -mt-10">
+      <div v-for="n in 10" :key="n" class="my-10">
+        <tuition-card></tuition-card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import TuitionCard from '@/components/TuitionCard.vue';
+
 export default {
   name: 'Tuitions',
+  components: {
+    TuitionCard,
+  },
 };
 </script>
-
-<style>
-
-</style>
