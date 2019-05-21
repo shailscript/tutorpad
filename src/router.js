3a758abc4a5e4b9da0cd013f22a79db9a9d53eb2
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-21 13:50:36 -0400

    @paths added in import statement

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
-import Finder from './views/Finder.vue';
-import Tuitions from './views/Tuitions.vue';
+import Home from '@/views/Home.vue';
+import Finder from '@/views/Finder.vue';
+import Tuitions from '@/views/Tuitions.vue';
 
 Vue.use(Router);
 
