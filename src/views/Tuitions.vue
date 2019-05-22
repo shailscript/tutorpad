fba6f248b6e260e563e20a91fc7b59291c25949f
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-22 11:59:38 -0400

    Added app action bar

--- src/views/Tuitions.vue
+++ src/views/Tuitions.vue
@@ -1,8 +1,15 @@
 <template>
-  <div class="h-full overflow-y-scroll">
-    <div class="container mx-auto -mt-10">
-      <div v-for="n in 10" :key="n" class="my-10">
-        <tuition-card></tuition-card>
+  <div class="h-full w-full">
+    <div class="py-2 sm:bg-grey-lighter">
+      <div class="container mx-auto px-4">
+        <app-action-bar title='Tuitions'></app-action-bar>
+      </div>
+    </div>
+    <div class="h-full overflow-y-scroll">
+      <div class="container mx-auto">
+        <div v-for="n in 10" :key="n" class="my-10">
+          <tuition-card></tuition-card>
+        </div>
       </div>
     </div>
   </div>
@@ -10,11 +17,13 @@
 
 <script>
 import TuitionCard from '@/components/TuitionCard.vue';
+import AppActionBar from '@/components/AppActionBar.vue';
 
 export default {
   name: 'Tuitions',
   components: {
     TuitionCard,
+    AppActionBar,
   },
 };
 </script>
