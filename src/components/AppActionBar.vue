88474d68e6abe17a3ccc40c8e4fb0d21e4f40fa3
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-23 23:11:00 -0400

    Characteristic styles coupled with action bar

--- src/components/AppActionBar.vue
+++ src/components/AppActionBar.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="flex justify-between items-center">
-    <h2>{{ title }}</h2>
-    <slot></slot>
+  <div class="py-2 px-4 sm:bg-grey-lighter">
+    <div class="container mx-auto">
+      <div class="flex justify-between items-center">
+        <h2>{{ title }}</h2>
+        <slot></slot>
+      </div>
+    </div>
   </div>
 </template>
 
